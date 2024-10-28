67f4efdb0f6d6fdba12d4610630287b1e0e29f19
Masha <mirmamenkov@gmail.com>
2024-10-28 23:57:27 +0300

    добавлена функция load_message

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
-#загружает промпт из папки /resources/prompts/
-
+# Загружает промпт из папки /resources/prompts/
 def load_prompt(name):
     with open("resources/prompts/" + name + ".txt", "r", encoding="utf8") as file:
+        return file.read()
+
+# Загружает промпт из папки /resources/message/
+def load_message(name):
+    with open("resources/message/" + name + ".txt", "r", encoding="utf8") as file:
         return file.read()
